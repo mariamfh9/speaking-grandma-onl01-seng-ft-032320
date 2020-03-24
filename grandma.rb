800cca09a50afdcf812dbf89288fe853e568b66b
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-24 19:02:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,13 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(string)
+  if string.upcase != true
+    return "NO, NOT SINCE 1938!"
+  elsif string == string.upcase
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "SPEAK UP, SONNY!"
+  end
+end 
