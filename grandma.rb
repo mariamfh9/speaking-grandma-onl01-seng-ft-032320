00b36ce66aa6195b5300d64977f178800885b877
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-24 19:04:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(string)
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
   else
-    return "SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end 
